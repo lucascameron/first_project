28505b732e4fe53730bc4a8ae269654bddecd552
lucascameron <lucas_cameron@hotmail.com>
2025-08-18 11:40:14 -0700

    Add files via upload

---
+++ First_Assignment.py
@@ -0,0 +1,61 @@
+#note note note
+
+#Problem 1
+
+# name = input("What is your name? ")
+
+# print("Hello " + name + ". My name is Jonathan! I am a computer!")
+
+#Problem 2
+
+#Solution 1
+# number = int(input("Input a number and I will tell you if it's even or odd! Enter here -> "))
+
+# even_or_odd = number % 2
+
+# if even_or_odd == 0:
+#     print(f"{number} is an even number!")
+# else:
+#     print(f"{number} is an odd number!")
+
+#Solution 2
+# number = int(input("Input a number and I will tell you if it's even or odd! Enter here -> "))
+
+# result = (number // 2) * 2
+
+# if result == number:
+#     print(f"{number} is an even number!")
+# else:
+#     print(f"{number} is an odd number!")
+
+
+
+#Problem 3
+
+for x in range(1,11):
+    print(x)
+
+#Problem 4
+
+
+#Problem 5
+
+
+
+#Problem 6
+
+
+
+#Problem 7
+
+
+
+#Problem 8
+
+
+
+#Problem 9
+
+
+
+#Problem 10
